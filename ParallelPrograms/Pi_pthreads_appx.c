06596bde36e0677d562e9079a244f2df3109c276
David Tippett <dtaivpp@Davids-MacBook.fios-router.home>
2017-12-14 21:58:35 -0500

    Got the program working using mutex locks.

--- ParallelPrograms/Pi_pthreads_appx.c
+++ ParallelPrograms/Pi_pthreads_appx.c
@@ -5,13 +5,16 @@
 #include <pthread.h>
 
 int thread_count;
-
-long long int number_of_tosses;
+long long int number_of_tosses = 0;
+long long int global_sum = 0; 
+pthread_mutex_t mutex;
 
 void* thread_worker(void* rank);
-void Get_Input(int, int, long long int*);
+void Get_Input();
 
 int main(int argc, char* argv[]){
+    Get_Input ();
+    
     long thread;
     pthread_t* thread_handles;
 
@@ -23,29 +26,23 @@ int main(int argc, char* argv[]){
         pthread_create(&thread_handles[thread],NULL,thread_worker,(void*)thread);
     }
 
-    long long int local_num = 0;
     for (thread = 0; thread < thread_count; thread++){
-        pthread_join(&thread_handles[thread], local_num);
-        global_num_in_circle += &local_num;
+        pthread_join(thread_handles[thread], NULL);
     }
 
     double pi_estimate = 0;
-    long long int global_num_in_circle = 0;
-
-        pi_estimate = 4 * global_num_in_circle / ((double)number_of_tosses * thread_count);
+        pi_estimate = 4 * (double)global_sum / ((double)(number_of_tosses * thread_count));
         printf("%.4f", pi_estimate);
 
-    MPI_Finalize();
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
 
-void Get_Input (int comm_sz, int my_rank, long long int* number_of_tosses){
-    if (my_rank == 0){
+void Get_Input (){
         fflush( stdout );
         printf("Input the number of darts for each process to throw\n");
-        scanf("%lld", number_of_tosses);
-    }
-    MPI_Bcast(number_of_tosses,1,MPI_LONG_LONG_INT,0,MPI_COMM_WORLD);
+        scanf("%lld", &number_of_tosses);
+    //MPI_Bcast(number_of_tosses,1,MPI_LONG_LONG_INT,0,MPI_COMM_WORLD);
 }
 
 void* thread_worker(void* rank){
@@ -56,12 +53,16 @@ void* thread_worker(void* rank){
     double y_local;
 
     long long int number_in_circle = 0;
-    double distance_squared = 0;
+    double distance_squared = 0;    
     for (long long int toss = 0; toss < number_of_tosses; toss++){
         x_local = -1 + rand() % 2;
         y_local = -1 + rand() % 2;
         distance_squared = x_local * x_local + y_local * y_local;
         (distance_squared <= 1) ? number_in_circle++ : number_in_circle;
     }
-    return *number_in_circle;
+    pthread_mutex_lock(&mutex);
+    global_sum += number_in_circle;
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
 }
