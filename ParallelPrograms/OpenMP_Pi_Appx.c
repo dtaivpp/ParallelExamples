c6992d73c405d5f109fb414bffe032f7ee849858
David Tippett <dtaivpp@Davids-MacBook.fios-router.home>
2017-12-15 19:20:42 -0500

    Initial Creation

--- ParallelPrograms/OpenMP_Pi_Appx.c
+++ ParallelPrograms/OpenMP_Pi_Appx.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include <mpi.h>
+#include <omp.h>
 #include <stdlib.h>
 #include <time.h>
 
-void Get_Input(int, int, long long int*);
+void Get_Input();
+long long int number_of_tosses = 0;
 
-int main(){
-    int comm_sz;
-    int my_rank;
+int main(int argc, char* argv[]){
+    int thread_count;
     double pi_estimate = 0;
-    long long int number_of_tosses;
     long long int global_num_in_circle = 0;
 
-    MPI_Init(NULL,NULL);
+    thread_count = strtol(argv[1], NULL, 10);
+
+    Get_Input();
+
+# pragma omp parallel num_threads(thread_count)
+{
     time_t t_local;
     srand((unsigned) time(&t_local));
-
-    //Initialize the size of comm messages
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-    //Initialize the rank number
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
-    Get_Input(comm_sz, my_rank, &number_of_tosses);
 
     double x_local;
     double y_local;
@@ -34,22 +31,19 @@ int main(){
         distance_squared = x_local * x_local + y_local * y_local;
         (distance_squared <= 1) ? number_in_circle++ : number_in_circle;
     }
-    MPI_Reduce(&number_in_circle,&global_num_in_circle,1,MPI_LONG_LONG_INT,MPI_SUM,0,MPI_COMM_WORLD);
 
-    if (my_rank == 0){
-        pi_estimate = 4 * global_num_in_circle / ((double)number_of_tosses * comm_sz);
-        printf("%.4f", pi_estimate);
-    }
+# pragma omp critical
+    global_num_in_circle += number_in_circle;
+}
 
-    MPI_Finalize();
+    pi_estimate = 4 * (double)global_num_in_circle / ((double)(number_of_tosses * thread_count));
+    printf("%.4f", pi_estimate);
+
     return 0;
 }
 
-void Get_Input (int comm_sz, int my_rank, long long int* number_of_tosses){
-    if (my_rank == 0){
+void Get_Input (){
         fflush( stdout );
         printf("Input the number of darts for each process to throw\n");
-        scanf("%lld", number_of_tosses);
-    }
-    MPI_Bcast(number_of_tosses,1,MPI_LONG_LONG_INT,0,MPI_COMM_WORLD);
+        scanf("%lld", &number_of_tosses);
 }
